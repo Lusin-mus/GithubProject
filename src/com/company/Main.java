30428213d708964772eeeb90b1fa199dc60a5808
Student <Student@EPAMYERW0046.moscow.epam.com>
2019-12-06 14:11:21 +0400

    New

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] array={2, 3, 4, 5, 89};
+	int sum= 0;
+	for (int i=0; i< array.length; i++){
+	    sum+=array[i];
+    }
+	System.out.println(sum);
+    }
+}
